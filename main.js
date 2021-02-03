7c1ff49a3462be333af7ee11c56588f6ba220be8
Eugenia <mariaedallasta@gmail.com>
2021-02-03 17:14:50 -0300

    Finish excercise

--- main.js
+++ main.js
@@ -1,17 +1,45 @@
-'use strict';
+"use strict";
 
-const buttonElement = document.querySelector('.js-btn try'); // boton de prueba
-const hintElement = document.querySelector('.js-hint');      // pistas
-const triesElement = document.querySelector('.js-tries');    // nto de intentos
-
+const userNumberElement = document.querySelector(".js-user-number"); // El nro que escribe la usuaria
+const buttonElement = document.querySelector(".js-button"); // boton Prueba
+const pTipElement = document.querySelector(".js-tip"); // el párrafo con las pistas
+const counterElement = document.querySelector(".js-counter");
 
 function getRandomNumber(max) {
-return Math.ceil(Math.random() * max);
+  return Math.ceil(Math.random() * max); //funcion para nro aleatorio
 }
 
-const RandomNumber = getRandomNumber(100)
-console.log(RandomNumber)
+function renderTip(message) {
+  pTipElement.innerHTML = "Pista: " + message;
+}
 
- 
+function handleClickButton(event) {
+  //funcion handle listener
 
+  const userNumberValue = userNumberElement.value;
+  const numberValue = parseInt(userNumberValue);
+
+  if (numberValue < 1 || userNumberValue > 100) {
+    renderTip("El número debe estar entre 1 y 100 🤯");
+  } else if (numberValue > randomNumber) {
+    renderTip("Demasiado alto 😋 !!!");
+  } else if (numberValue < randomNumber) {
+    renderTip("Demasiado bajo 😱 !!!");
+  } else {
+    renderTip("Has ganado campeona 😀!!!");
+  }
+}
+
+function handleClickCounter(event) {
+  counter = counter + 1;
+  counterElement.value = "Número de intentos: " + counter;
+}
+
+const randomNumber = getRandomNumber(100);
+console.log(randomNumber);
+
+let counter = 0;
+
+buttonElement.addEventListener("click", handleClickButton);
+buttonElement.addEventListener("click", handleClickCounter);
 
